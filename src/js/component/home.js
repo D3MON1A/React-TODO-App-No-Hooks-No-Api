f5ed732cf2ace54f517251c84d553d8098fd092e
leisyarcia <leisyarcia@gmail.com>
2020-10-09 14:22:22 +0000

    first commit

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,65 @@
 import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+export class Home extends React.Component {
+	//input field with enter key submission
+	//array to hold tasks
+	//map the array - for each element, display it in an <li>
+	//add delete functionality - with event listener onclick
+	//delete icon - to show on hover
+	//when no tasks: display "No tasks, add a task"
 
-//create your first component
-export function Home() {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	constructor() {
+		super();
+		this.state = {
+			tasks: ["First task", "Second task"],
+			inputTask: ""
+		};
+	}
+
+	saveTask = e => {
+		if (e.keyCode == 13) {
+			let newTasks = this.state.tasks.concat(e.target.value);
+			this.setState({
+				tasks: newTasks,
+				inputTask: ""
+			});
+		}
+	};
+
+	deleteTask = id => {
+		const updatedTasks = this.state.tasks.filter(
+			task => task != this.state.tasks[id]
+		);
+		this.setState({ tasks: updatedTasks });
+	};
+
+	render() {
+		return (
+			<div className="to-do-sheet text-center mt-5">
+				<h1>TO DO:</h1>
+				<input
+					autoFocus={true}
+					placeholder="What needs to be done?"
+					value={this.state.inputTask}
+					onChange={e => this.setState({ inputTask: e.target.value })}
+					onKeyUp={this.saveTask}
+				/>
+				<ul>
+					{this.state.tasks.map((task, i) => {
+						return (
+							<li key={i}>
+								{task}{" "}
+								<span
+									type="button"
+									onClick={() => this.deleteTask(i)}>
+									{" "}
+									x{" "}
+								</span>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		);
+	}
 }
